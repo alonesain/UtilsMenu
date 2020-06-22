4c840b5f71dacdf26700c1529609416617a6e362
AloneSain <66701651+alonesain@users.noreply.github.com>
2020-06-22 21:57:37 +0600

    Update mum.py

--- mum.py
+++ mum.py
@@ -8,21 +8,20 @@ print(Fore.RED + "MoreUtilsMenu")
 print(Fore.RED + "Author: alonesain")
 print(Fore.GREEN + """#BOMBERS#
 [1]-b0mb3r
-[2]-smsham
-[3]-Parad1se Bomber
-[4]-HamBom
+[2]-smsham 
+[3]-HamBom
 #PHISHING#
-[5]-kingfish
-[6]-kingfish2.0
-[7]-kingfish3.0
-[8]-NexPhisher
+[4]-kingfish
+[5]-kingfish2.0
+[6]-kingfish3.0
+[7]-NexPhisher
 #TOOlS#
-[9]-Toolss
+[8]-Toolss
 #DDOS#
-[10]-HULK(min 1gb ram)
-[11]-Aiohttpdos
+[9]-HULK(min 1gb ram)
+[10]-Aiohttpdos
 #HTTP#
-[12]-NGROK or LocalHost.run""")
+[11]-NGROK or LocalHost.run""")
 
 os.chdir('Utilits')
 
@@ -39,30 +38,25 @@ while True:
 		os.system("python3 smsham.py")
 	elif num == ('3'):
 		os.system("clear")
-		print(Fore.RED + "Starting Parad1se Bomber")
-		os.chdir("Parad1seBomb")
-		os.system("python3 main.py")
-	elif num == ('4'):
-		os.system("clear")
 		print(Fore.RED + "Starting HamBom")
 		os.chdir("hambom")
 		os.system("python3 hambom.py")
-	elif num == ('5'):
+	elif num == ('4'):
 		os.system("clear")
 		print(Fore.CYAN + "Starting kingfish")
 		os.chdir("kingfish")
 		os.system("python3 fsh.py")
-	elif num == ('6'):
+	elif num == ('5'):
 		os.system("clear")
 		print(Fore.CYAN + "Starting kingfish2.0")
 		os.chdir("kingfish2.0")
 		os.system("python3 fsh.py")	
-	elif num == ('7'):
+	elif num == ('6'):
 		os.system("clear")
 		print(Fore.CYAN + "Starting kingfish3.0")
 		os.chdir("kingfish3.0")
 		os.system("python3 fsh.py")
-	elif num == ('8'):
+	elif num == ('7'):
 		os.system("clear")
 		print(Fore.CYAN + "Starting NexPhisher")
 		print(Fore.CYAN + "[1]-Install and start [2]-Start")
@@ -85,22 +79,22 @@ while True:
 				os.system("./nexphisher")
 		elif nex == ('2'):
 			os.system("bash nexphisher")
-	elif num == ('9'):
+	elif num == ('8'):
 		os.system("clear")
 		print(Fore.MAGENTA + "Starting Toolss")
 		os.chdir("toolss")
 		os.system("python3 Toolss.py")
-	elif num == ('10'):
+	elif num == ('9'):
 		os.system("clear")
 		print(Fore.GREEN + "Starting HULK")
 		os.chdir("hulky")
 		os.system("python3 hulky.py")
-	elif num == ('11'):
+	elif num == ('10'):
 		os.system("clear")
 		print(Fore.GREEN + "Starting aiohttpdos")
 		os.chdir("aiohttpdos")
 		os.system("python3 aiohttpdos.py")
-	elif num == ('12'):
+	elif num == ('11'):
 		print(Fore.RED + '[1]-Ngrok [2]-SSH LocalHost')
 		http =str(input())
 		if http == ('1') :
