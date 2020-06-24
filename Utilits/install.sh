957c24ee99b1ae459db43d2443bd9f6496a278a7
AloneSain <66701651+alonesain@users.noreply.github.com>
2020-06-25 02:18:37 +0600

    Update install.sh

--- Utilits/install.sh
+++ Utilits/install.sh
@@ -1,26 +1,20 @@
 apt update && apt upgrade
-apt install git -y
-apt install python -y
+apt install git 
+apt install python
 apt install python2
 apt install php
 apt install openssh
 pip install termcolor
-pkg install wget -y
-pkg install php -y
-pkg install python -y
-pkg install python2 -y
-python3 -m pip install vk
-python3 -m pip install vk_api
-python3 -m pip install colorama
-python3 -m pip install bs4
-python3 -m pip install pysocks
-python3 -m pip install prettytable
-python3 -m pip install -r requirements.txt
-alias umenu="cd ~/UtilsMenu;python3 mum.py"
-
-cd HOME/UtilsMenu/Utilits
-
-git clone https://github.com/crinny/b0mb3r.git
+pip install b0mb3r
+pkg install wget
+pkg install php
+pkg install python
+pkg install python2
+pip install vk
+pip install vk_api
+pip install bs4
+pip install pysocks
+pip install prettytable
 git clone https://github.com/termux-lab/smsham.git 
 git clone https://github.com/4D4N-Termux/hambom.git
 git clone https://github.com/termux-lab/kingfish.git
@@ -29,9 +23,3 @@ git clone https://github.com/termux-lab/kingfish3.git
 git clone https://github.com/htr-tech/nexphisher.git
 git clone https://github.com/AnonHackerr/toolss.git
 git clone https://github.com/4D4N-Termux/aiohttpdos.git
-
-cd b0mb3r
-python3 setup.py
-
-umenu
-
